067a1a91114020b26991f7bd7e6f30d07d0fd3be
Marieta <maria.it3@gmail.com>
2020-09-14 16:24:36 +0100

    Adding readme

--- src/main/java/ArraysLeftRotation.java
+++ src/main/java/ArraysLeftRotation.java
@@ -1,39 +1,3 @@
-/*
-https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
-* A left rotation operation on an array shifts each of the array's elements  unit to the left.
-For example,if left rotations are performed on array , then the array would become.
-Given an array of integers and a number, perform  left rotations on the array. Return the updated array
-to be printed as a single line of space-separated integers.
-
-Function Description:
-It should return the resulting array of integers.
-The method should have the following parameter(s):
-- An array of integers
-- An integer (the number of rotations)
-
-Input Format:
-The first line contains two space-separated integers:
-- n: size of tha array
-- d: number of left rotations
-The second line contains  space-separated integers .
-
-Constraints:
-- 1 <= n <= 10^5
-- 1 <= d <= n
-- 1 <= a[i] <= 10^6
-
-Output Format:
-Print a single line of  space-separated integers denoting the final state of the array after performing left rotations.
-
-Sample Input:
-5 4
-1 2 3 4 5
-
-Sample Output:
-5 1 2 3 4
-
-* */
-
 public class ArraysLeftRotation {
 
     public static int[] arraysLeftRotation(int[] a, int d) {
